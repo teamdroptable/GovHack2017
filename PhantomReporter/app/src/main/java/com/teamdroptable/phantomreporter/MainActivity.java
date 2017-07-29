79f8a3b3eb046648447e40d0bd57938612d5d652
Patrick McCawley <urainium235@gmail.com>
2017-07-29 22:55:00 +1000

    google map API setup

--- PhantomReporter/app/src/main/java/com/teamdroptable/phantomreporter/MainActivity.java
+++ PhantomReporter/app/src/main/java/com/teamdroptable/phantomreporter/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.app.ActivityCompat;
+import android.text.Layout;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+
 
 
         final Button policeButton = (Button) findViewById(R.id.bttnPolice);
@@ -43,7 +46,10 @@ public class MainActivity extends AppCompatActivity {
 
                 startActivity(callIntent);
             }
+
         });
+
+
 
 
     }
